Update Gaussian and galaxy fields for Planck and w0wa models; fix normalization and bias expansion

src/field_power_spectrum.py
@@ -14,7 +14,7 @@ def extract_redshift(filename):
         return None
 
 def compute_power_spectrum(field, box_size):
-    field_k = np.fft.fftn(field)    # real space to Fourier space: δ(k)
+    field_k = np.fft.fftn(field, norm="forward")    # real space to Fourier space: δ(k)
     power = np.abs(field_k)**2  # P(k) = |δ(k)|^2
 
     # k-grid
@@ -43,7 +43,7 @@ def compute_power_spectrum(field, box_size):
 
     k_min = k_mag_nonzero.min()
     k_max = np.pi * n_grid / box_size  # Nyquist frequency
-    k_bins = np.logspace(np.log10(k_min), np.log10(k_max), num=6)
+    k_bins = np.logspace(np.log10(k_min), np.log10(k_max), num=7)
 
 
     # Bin power spectrum by |k| to get P(k)

src/galaxy_bias_expansion.py
@@ -66,7 +66,7 @@ def galaxy_bias_field(delta, box_size, b1, b2, bG2, n_bar):
 
     # Bias operators in Lagrangian space
     delta_squared = delta**2
-    delta_squared -= np.mean(delta_squared) # Eq. 8 and 9 
+    delta_squared -= np.mean(delta_squared) # Eq. 8 and 9
 
     # Tidal operator G2
     kf = 2 * np.pi / box_size   
@@ -108,6 +108,8 @@ def galaxy_bias_field(delta, box_size, b1, b2, bG2, n_bar):
         epsilon = np.random.normal(loc=0.0, scale=noise_std, size=delta.shape)  # scale such that P(k) is 1/n_bar; 3d grid.
         delta_h += epsilon  # stochastic component
 
+    delta_h -= np.mean(delta_h)  # Remove mean to avoid bias
+    
     return delta_h
 
 def main():
@@ -124,9 +126,9 @@ def main():
     output_dir = os.path.join(model_dir, "galaxy_field")
     os.makedirs(output_dir, exist_ok=True)
 
-    box_size = 500.0
-    b1, b2, bG2 = 1.5, 0.5, 0.5   #adjustable bias parameters
-    n_bar = None
+    box_size = 1000.0
+    b1, b2, bG2 = 1.2, -0.405, -0.127   #b1 from DESI 2016 BSG Figure 3.4, scaling from Chen et al. (2019)
+    n_bar = 1e-3  # DESI DR2 BGS number density Figure 3
 
     for filename in sorted(os.listdir(input_dir)):
         if filename.endswith(".npy"):

src/generate_gaussian_field.py
@@ -7,6 +7,8 @@ from scipy.interpolate import interp1d
 def generate_gaussian_field(pk_file, box_size=1000.0, n_grid=256): #Box size in Mpc/h; 128^3 grid points
     data = np.loadtxt(pk_file)  # Load P(k)
     k_vals, pk_vals = data[:, 0], data[:, 1]
+    volume = box_size**3
+    pk_vals /= volume
 
     # Interpolation: don't crash outside k_vals range but assume P(k)=0
     pk_interp = interp1d(k_vals, pk_vals, bounds_error=False, fill_value=0) 
@@ -27,7 +29,11 @@ def generate_gaussian_field(pk_file, box_size=1000.0, n_grid=256): #Box size in 
     field_k = noise * amplitude #correctly scaled noise
     
     field_k[0, 0, 0] = 0.0  # Set zero mode to 0 (remove biased background)
-    field_real = np.fft.ifftn(field_k).real # Inverse FFT to get real-space field
+    field_real = np.fft.ifftn(field_k, norm="forward").real # Inverse FFT to get real-space field
+
+    print("Field variance (real space):", np.var(field_real))
+    print("Field mean (real space):", np.mean(field_real))
+    print("Typical P(k):", np.median(pk_interp(k_mag)))
 
     return field_real
 